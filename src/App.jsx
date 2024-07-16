5c5d9d6a2ee6d9f696723d959c77209f6a4987ca
Diego-Paul-Villavicencio <diegopaulvillavicencioordonez@gmail.com>
2024-07-16 11:09:07 +0000

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CodeRepository from './CodeRepository.jsx';
 import ColorTransition from './ColorTransition.jsx';
 import Access from './Access.jsx';
+import TextCredits from './Credits.jsx';
 import './App.css';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <CodeRepository />
         <ColorTransition />
         <Access />
+        <TextCredits />
         </>
     );
 }
---
+++ src/Credits.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextCredits = () => {
+  return (
+    <>
+      <h6 className='text-credits'>COMPONENTES FRONTEND es una aplicación web diseñada por Diego Villavicencio</h6>
+    </>
+  );
+}
+
+export default TextCredits;
